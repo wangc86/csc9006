de18d2dfddbae10b906bc3de9ebf93e091b13eab
Chao Wang <cw@ntnu.edu.tw>
2020-06-04 08:40:54 +0800

    polishing figures

--- lab3/plot_scripts/CPU_utilization.py
+++ lab3/plot_scripts/CPU_utilization.py
@@ -16,9 +16,12 @@ def mean_confidence_interval(data, confidence=0.95):
 
 #majorColor='#5e3c99'
 #minorColor='#fdb863'
-majorColor='k'
-minorColor='r'
-minorColor2='b'
+#majorColor='k'
+#minorColor='r'
+#minorColor2='b'
+majorColor='#dc267f'
+minorColor='#648fff'
+minorColor2='#ffb000'
 
 
 #import matplotlib as mpl
@@ -37,9 +40,14 @@ fig1, ax1 = plt.subplots()
 
 exp_path = '../makeup_exp/'
 
-s = ['EDF', 'RM', 'FIFO']
-m = ['o', 'x', 's']
-c = [majorColor, minorColor, minorColor2]
+#s = ['EDF', 'RM', 'FIFO']
+#m = ['o', 'x', 's']
+#c = [majorColor, minorColor, minorColor2]
+#l = ['-', '--', ':']
+s = ['FIFO', 'RM', 'EDF']
+m = ['d', 'x', 'o']
+c = [minorColor2, minorColor, majorColor]
+l = [':', '--', '-']
 for x in range(0,3,1):
     nOut = []
     nOut_err = []
@@ -50,7 +58,7 @@ for x in range(0,3,1):
         nOut.append(y)
         nOut_err.append(yerr)
     ax1.errorbar([1.5, 4.5, 7.5, 10.5, 13.5], nOut,
-      yerr=nOut_err, marker=m[x], color=c[x], lw=0.7, capthick=0.7, capsize=2, mec=c[x], ms=4, mew=1, mfc='none')
+      yerr=nOut_err, marker=m[x], color=c[x], lw=1.7, linestyle=l[x], capthick=0.7, capsize=2, mec=c[x], ms=4, mew=1, mfc='none')
 
 sns.despine()
 plt.xlim(0,15)
@@ -61,12 +69,12 @@ ax1.set_ylim(0,105)
 ax1.set_xlabel('Number of topics')
 ax1.set_ylabel('CPU utilization (%)')
 
-h_EDF = lines.Line2D([],[],linewidth=.7,linestyle='-',color=majorColor,marker='o', lw=0.7, mec=majorColor, ms=4, mew=1, mfc='none')
-h_RM = lines.Line2D([],[],linewidth=.7,linestyle='-',color=minorColor,marker='x', lw=0.7, mec=minorColor, ms=4, mew=1, mfc='none')
-h_FIFO = lines.Line2D([],[],linewidth=.7,linestyle='-',color=minorColor2,marker='s', lw=0.7, mec=minorColor2, ms=4, mew=1, mfc='none')
+h_EDF = lines.Line2D([],[],linewidth=1.7,linestyle='-',color=majorColor,marker='o', lw=0.7, mec=majorColor, ms=4, mew=1, mfc='none')
+h_RM = lines.Line2D([],[],linewidth=1.7,linestyle='--',color=minorColor,marker='x', lw=0.7, mec=minorColor, ms=4, mew=1, mfc='none')
+h_FIFO = lines.Line2D([],[],linewidth=1.7,linestyle=':',color=minorColor2,marker='d', lw=0.7, mec=minorColor2, ms=4, mew=1, mfc='none')
 
-plt.legend((h_EDF,h_RM,h_FIFO),('EDF','RM','FIFO'),loc=2,
-            borderaxespad=0.1)
+plt.legend((h_EDF,h_RM,h_FIFO),('EDF','RM','FIFO'),loc=2)#,
+#            borderaxespad=0.1)
 #plt.tight_layout()
 plt.show()
 #plt.savefig('../../exp/exp-prioritization/exp2-utilization.pdf',
