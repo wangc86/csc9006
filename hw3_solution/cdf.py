a27adb31f76bf4f5b333b3ed778a6e107fa822ce
Chao Wang <cw@ntnu.edu.tw>
2021-04-28 11:27:35 +0800

    adding homework3 solution

---
+++ hw3_solution/cdf.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#rt1 = np.loadtxt('./priority_inversion.out')
+#rt1 = np.loadtxt('./pip.out')
+rt1 = np.loadtxt('./pcp.out')
+
+fig1, ax1 = plt.subplots()
+
+n = np.arange(1,len(rt1)+1) / np.float(len(rt1)) * 100
+rtSorted = np.sort(rt1)
+ax1.step(rtSorted, n, color='k', label='high-priority task')
+
+ax1.set_xlabel('Response Time (milliseconds)')
+ax1.set_ylabel('Percentage (%)')
+plt.xlim(xmin=0)
+
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
